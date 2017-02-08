42805e0221c777e810ccdf4e39b46d5c11da42cf
Viktor Ohád <viktor.ohad@gmail.com>
2017-02-08 09:22:15 +0000

    Final app

--- server.js
+++ server.js
@@ -1,39 +1,58 @@
-var express = require("express");
-var url = require("url");
-var app = express();
-var moment = require("moment");
+const express = require("express");
+const moment = require("moment");
+const app = express();
 
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+const formats = [
+    
+    'MMMM D, YYYY',
+    'MMMM D YYYY',
+    'MMM D, YYYY',
+    'MMM D YYYY',
+    'D MMMM YYYY',
+    'D MMM YYYY',
+    'D YYYY MMMM',
+    'D YYYY MMM',
+    'YYYY MM DD',
+    'YYYY MMMM DD',
+    'YYYY MMMM D'
+  ];
 
 app.all("*", function(request, response, next) {
   response.writeHead(200, { "Content-Type": "application/json" });
   next();
 });
 
-app.get("/", function(request, response) {
-  response.end(new Date().toDateString());
+app.get("/", function(req, res) {
+  res.end(new Date().toDateString());
 });
 
-app.get('*', function(req, res){
-  var data = url.parse(req.url, false);
-  var date = data.pathname.replace(/%/g , ' ').replace('/', '');
-  if(moment(date, ["MM-DD-YYYY", "YYYY-MM-DD", "DD-MM-YYYY", "YYYY.MM.DD", "MMMM-DD-YYYY"], true).isValid()){
+app.get('/:time', function(req, res){
+  
+  var date = req.params;
+  
+  if(moment(date.time, formats, true).isValid()){
     res.end(JSON.stringify({
-      unix : Date.parse(date)/1000,
-      normal : date
+      unix : moment(date.time).valueOf(),
+      natural : moment(date.time).format("YYYY MMMM, DD")
     }));
   }
-  else if(Number(data.pathname.replace('/', '')) > 0){
+  else if(isNumeric(date.time)){
     res.end(JSON.stringify({
-      unix : data.pathname.replace('/', ''),
-      normal: moment.unix(data.pathname.replace('/', '')).format("YYYY-MM-DD")
+      unix : date.time,
+      natural: moment.unix(date.time).format("YYYY MMMM, DD")
     }));
   }
   else{
     res.end(JSON.stringify({
       unix : null,
-      normal : null
+      natural : null
     }));
   }
+  console.log((date.time));
 });
 
 app.listen(8080, function () {
